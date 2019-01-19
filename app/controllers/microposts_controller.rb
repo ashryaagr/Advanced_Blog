16e3be3ea4675c32b527ae802c842e1a683c1278
ashryaagr <ashryaagr@gmail.com>
2019-01-19 19:30:58 +0530

    separate page for posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,9 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
+  def index
+    @microposts = Micropost.paginate(page: params[:page])
+  end
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
+  get '/posts', to: 'microposts#index'
   resources :users
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
