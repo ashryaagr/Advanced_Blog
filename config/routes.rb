05b74a55b7102a38cf7d6e7b7730c82046f75f8b
ashryaagr <ashryaagr@gmail.com>
2019-01-19 18:09:02 +0530

    Microposts feature added

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get '/logout',  to: 'sessions#destroy'
   resources :users
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
