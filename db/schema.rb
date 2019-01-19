0edf677ec83b6b9f775bad283e3b1fe6ce296a71
ashryaagr <ashryaagr@gmail.com>
2019-01-19 16:59:07 +0530

    micropost facility added

--- db/schema.rb
+++ db/schema.rb
@@ -12,27 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_01_19_110214) do
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -50,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_01_19_110214) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "perishable_token"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
